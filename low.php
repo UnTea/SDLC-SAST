f0752181ede8428693fc77130ea67d5376f8d787
UnTea <weqqu2014@yandex.ru>
2021-11-29 15:20:26 +0300

    add fixed code

--- low.php
+++ low.php
@@ -1,24 +1,18 @@
 <?php
 
 if( isset( $_GET[ 'Submit' ] ) ) {
-    // Get input
     $id = $_GET[ 'id' ];
 
-    // Check database
     $getid  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-    $result = mysqli_query($GLOBALS["___mysqli_ston"],  $getid ); // Removed 'or die' to suppress mysql errors
+    $result = mysqli_query($GLOBALS["___mysqli_ston"],  $getid );
 
-    // Get results
-    $num = @mysqli_num_rows( $result ); // The '@' character suppresses errors
+    $num = @mysqli_num_rows( $result );
     if( $num > 0 ) {
-        // Feedback for end user
         echo '<pre>User ID exists in the database.</pre>';
     }
     else {
-        // User wasn't found, so the page wasn't!
         header( $_SERVER[ 'SERVER_PROTOCOL' ] . ' 404 Not Found' );
 
-        // Feedback for end user
         echo '<pre>User ID is MISSING from the database.</pre>';
     }
 
